757c7c309fb658ddfa49ce2a5b5e8a70e45f2f87
guilherme-wecont <71101049+guilherme-wecont@users.noreply.github.com>
2025-03-26 16:09:23 -0300

    extract separate audio

--- main.py
+++ main.py
@@ -21,22 +21,6 @@ app.add_middleware(
 class VideoRequest(BaseModel):
     url: str
 
-@app.post("/download")
-async def download_reel(data: VideoRequest):
-    url = data.url
-    try:
-        command = [
-            "yt-dlp",
-            "-g",
-            "--cookies", "cookies.txt",
-            url
-        ]
-        result = subprocess.check_output(command)
-        video_url = result.decode().strip().split("\n")[0]
-        return {"video_url": video_url}
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"detail": str(e)})
-
 @app.post("/process-video")
 async def process_video(data: VideoRequest):
     url = data.url
@@ -46,23 +30,27 @@ async def process_video(data: VideoRequest):
         os.makedirs(temp_dir, exist_ok=True)
 
         video_path = f"{temp_dir}/video.mp4"
-        audio_path = f"{temp_dir}/audio.wav"
+        audio_path = f"{temp_dir}/audio.m4a"
+        audio_wav_path = f"{temp_dir}/audio.wav"
         image1_path = f"{temp_dir}/thumb1.jpg"
         image2_path = f"{temp_dir}/thumb2.jpg"
 
+        # Baixar vídeo
         subprocess.run([
-            "yt-dlp",
-            "-f", "bv*+ba/b",
-            "--cookies", "cookies.txt",
-            "-o", video_path,
-            url
+            "yt-dlp", "-f", "bv[ext=mp4]", "-o", video_path, "--cookies", "cookies.txt", url
         ], check=True)
 
+        # Baixar áudio
         subprocess.run([
-            'ffmpeg', '-i', video_path, '-vn',
-            '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '2', audio_path
-        ], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            "yt-dlp", "-f", "ba", "-o", audio_path, "--cookies", "cookies.txt", url
+        ], check=True)
 
+        # Converter áudio m4a para wav
+        subprocess.run([
+            "ffmpeg", "-i", audio_path, "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "2", audio_wav_path
+        ], check=True)
+
+        # Captura de imagens do vídeo
         subprocess.run(["ffmpeg", "-i", video_path, "-ss", "00:00:01.000", "-vframes", "1", image1_path], check=True)
         subprocess.run(["ffmpeg", "-i", video_path, "-ss", "00:00:02.500", "-vframes", "1", image2_path], check=True)
 
@@ -74,10 +62,8 @@ async def process_video(data: VideoRequest):
             ]
         }
 
-    except subprocess.CalledProcessError as e:
-        return JSONResponse(status_code=500, content={"detail": f"FFmpeg error: {e.stderr.decode()}"})
     except Exception as e:
-        return JSONResponse(status_code=500, content={"detail": str(e)})
+        return JSONResponse(status_code=500, content={"detail": f"FFmpeg error: {str(e)}"})
 
 @app.get("/static/{uid}/{filename}")
 async def serve_static(uid: str, filename: str):
